ece6ff89a34db138ffdb9d400f952635f679947e
lourdes222 <lourdes.munoz666222@gmail.com>
2026-05-11 19:51:31 -0300

    primer commit

---
+++ js/eventos.js
@@ -0,0 +1,33 @@
+// este archivo responde a los evento del DOM,
+// interactuando con el usuario y el modelo de datos desarrollado en modulos.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    //cargamos listado de personas
+
+
+    //Mostrar formulario para agregar Persona desde el boton "Agregar Persona"
+ 
+
+    //Ocultar formulario "Agregar persona" desde el boton "cerrar" o X
+   
+
+    
+    //evento "submit" formulario "AgregarPersona"
+ 
+
+    //----------------------------------------------------------------------------------------------------------
+    //El evento de mostraFormularioModificar y cerrar formulario queda dentro de la funcion mostrarPersonas
+    //porque pueden ser varios botones y es necesario individualizarlos para capturar los datos de cada persona
+    //lo mismo para el boton eliminar queda dentro de la funcion mostrarPersonas
+    //----------------------------------------------------------------------------------------------------------
+
+
+    //evento "submit" formulario buscar
+   
+
+    //boton limpiar filtros
+   
+
+    //evento "submit" formulario modificar
+   
+})
