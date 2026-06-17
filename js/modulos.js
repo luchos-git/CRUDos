3459de61ca600f0475cdc6e6cd952828255d0beb
luchos-git <trevissan.luciano@gmail.com>
2026-06-17 20:14:18 -0300

    estoy arrancando

--- js/modulos.js
+++ js/modulos.js
@@ -46,8 +46,8 @@ const mostrarPersonas=(personasEncontradas)=>{
             <p>Dni: ${p.dni}</p>
         </div>
         <div class="botones">
-            <button class="btn-modificar">Modificar</button>
-            <button class="btn-eliminar">Eliminar</button>
+            <button class="btn-modificar" onclick = "mostrarFormModificar('${p.nombre}', ${p.edad}, ${p.dni})">Modificar</button>
+            <button class="btn-eliminar" onclick="eliminarPersona(${p.dni})">Eliminar</button>
         </div>
         </div>`
     })
@@ -69,9 +69,32 @@ const buscarPersona = (personaABuscar) => {
 
 
 //funcion para eliminar un registro de persona segun el DNI
+const eliminarPersona = (dni) => {
+    if(confirm("seguro desea eliminar?")){
+    personas = JSON.parse(localStorage.getItem("personas"));
+    indice = personas.findIndex(p=>p.dni==dni);
+    personas.splice(indice, 1);
+    localStorage.setItem("personas", JSON.stringify(personas));
+    mostrarPersonas();
+    mostrarMensaje(`registro eliminado`);
+    }
+}
 
 
 //funcion intermedia para modificar datos, carga datos guardados de una persona en el formulario y lo muestra
+const mostrarFormModificar = (nombreActual,edadActual, dni)=>{
+    console.log(nombreActual, edadActual, dni);
+
+    document.querySelector('#form-modificar').innerHTML=`
+    <div class="cerrar"><button class="btn-cerrar-formModif">X</button></div>
+                    <h3>Modificar datos persona DNI ${dni}</h3>
+                    <p> Ingresar nombre y/o edad a modificar </p>
+                    <input type="hidden" name="dni" value="${dni}">
+                    <input type="text" name="nombre" value="${nombreActual}" required>
+                    <input type="text" name="edad" value="${edadActual}" required>
+                    <input type="submit" name="submit" value="Modificar">`
+    document.querySelector('#form-modificar').style.display = 'block';
+}
 
 
 //funcion modificar Datos de una persona
